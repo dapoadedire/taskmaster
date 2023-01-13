03ada192f2f847cb4d578d20d25f6df548546345
dapoadedire <adedireadedapo19@gmail.com>
2023-01-13 15:36:11 +0100

    nice on desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, div } from "react";
 import "./App.css";
 import { nanoid } from "nanoid";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAdd,
+  faTrashCan,
+  faArrowDown,
+  faArrowUp,
+} from "@fortawesome/free-solid-svg-icons";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 function App() {
+  const [parent, enableAnimations] = useAutoAnimate(/* optional config */)
   const storedTodos = JSON.parse(localStorage.getItem("todos"));
- 
+
   if (storedTodos === null) {
     localStorage.setItem("todos", JSON.stringify([]));
   }
@@ -14,8 +25,6 @@ function App() {
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
-
-
 
   function addTodo(todo) {
     setTodo("");
@@ -44,71 +53,150 @@ function App() {
     setTodos(newTodos);
   }
 
+  function moveUp(todo) {
+    const currentIndex = todos.findIndex((item) => item.id === todo.id);
+    if (currentIndex > 0) {
+      const newTodos = [...todos];
+      newTodos.splice(currentIndex, 1);
+      newTodos.splice(currentIndex - 1, 0, todo);
+      setTodos(newTodos);
+    }
+  }
+
+  function moveDown(todo) {
+    const currentIndex = todos.findIndex((item) => item.id === todo.id);
+    if (currentIndex < todos.length - 1) {
+      const newTodos = [...todos];
+      newTodos.splice(currentIndex, 1);
+      newTodos.splice(currentIndex + 1, 0, todo);
+      setTodos(newTodos);
+    }
+  }
+
   return (
-    <div className="todo-container">
-      <h1>To-Do List</h1>
-      <form>
-        <input
-          type="text"
-          value={todo}
-          placeholder="Add a new task"
-          onChange={(e) => setTodo(e.target.value)}
-        />
+    <div className="font-mono">
+      <header className="h-32">
+        <h1 className="text-center flex justify-center items-center h-full font-extrabold text-5xl">Todo List</h1>
+      </header>
+      <main>
+        <div className="w-1/2 mx-auto" ref={parent}>
+          <form
+            className="flex mb-20"
+            onSubmit={(e) => {
+              e.preventDefault();
+              addTodo(todo);
+            }}
+          >
+            <input
+              type="text"
+              value={todo}
+              placeholder="Add a new task"
+              onChange={(e) => setTodo(e.target.value)}
+              required="false"
+              className="flex-grow rounded-lg border-gray-400 border-2 p-3"
+            />
 
-        <button
-          type="submit"
-          onClick={(e) => {
-            e.preventDefault();
-            addTodo(todo);
-          }}
-        >
-          Add Task
-        </button>
-      </form>
+            <button
+              type="submit"
+              className="rounded-lg border-gray-400 border-2 ml-3 p-3 w-20 font-bold"
+            >
+              <FontAwesomeIcon icon={faAdd} className="font-bold" />
+            </button>
+          </form>
 
-      <div>
-        <h3>Pending Tasks</h3>
-        {todos.filter((todo) => todo.done === false).length ? (
-          <ul>
-            {todos
-              .filter((todo) => todo.done === false)
-              .map((todo, index) => (
-                <li key={index} className="todo-item">
-                  <span className={todo.done ? "done" : ""}>{todo.todo}</span>
-                  <div className="todo-btns">
-                    <button onClick={() => markDone(todo)}>Mark as Done</button>
-                    <button onClick={() => deleteTodo(todo)}>Delete</button>
-                  </div>
-                </li>
-              ))}
-          </ul>
-        ) : (
-          <p>You have no pending tasks. Why not add one?</p>
-        )}
-      </div>
+          <div ref={parent} className="rounded-lg border-gray-300 border-2 mb-10 p-4">
+            <h3 className="text-center py-4 underline font-bold text-lg">
+              Pending Tasks
+            </h3>
+            {todos.filter((todo) => todo.done === false).length ? (
+              <ul ref={parent}>
+                {todos
+                  .filter((todo) => todo.done === false)
+                  .map((todo, index) => (
+                    <li
+                      key={index}
+                      className="rounded border-gray-200 border-2 my-4 flex p-3 align-center"
+                    >
+                      <FontAwesomeIcon
+                        icon={faArrowUp}
+                        onClick={() => moveUp(todo)}
+                        className="text-teal-600 cursor-pointer mx-1 p-3"
+                      />
+                      <FontAwesomeIcon
+                        icon={faArrowDown}
+                        onClick={() => moveDown(todo)}
+                        className=" text-teal-600 cursor-pointer mx-1 p-3"
+                      />
+                      <span className="flex-grow mx-1 p-3">{todo.todo}</span>
 
-      <div>
-        <h3>Completed Tasks</h3>
-        {todos.filter((todo) => todo.done).length ? (
-          <ul>
-            {todos
-              .filter((todo) => todo.done)
-              .map((todo, index) => (
-                <li key={index} className="todo-item completed">
-                  <span>{todo.todo}</span>
-                  <div className="todo-btns">
-                    <button onClick={() => markDone(todo)}>
-                      Mark as Incomplete
-                    </button>
-                    <button onClick={() => deleteTodo(todo)}>Delete</button>
-                  </div>
-                </li>
-              ))}
-          </ul>
-        ) : (
-          <p>You have not completed any tasks yet.</p>
-        )}
-      </div>
+                      <FontAwesomeIcon
+                        icon={faCheck}
+                        onClick={() => markDone(todo)}
+                        className="ml-2 text-teal-600 w-5 cursor-pointer mx-1 p-3"
+                      />
+
+                      <FontAwesomeIcon
+                        icon={faTrashCan}
+                        onClick={() => deleteTodo(todo)}
+                        className="mx-1 p-3 border-red-600 text-red-600"
+                      />
+                    </li>
+                  ))}
+              </ul>
+            ) : (
+              <p className="rounded border-gray-200 border-2 my-4 flex p-3">
+                You have no pending tasks. Why not add one?
+              </p>
+            )}
+          </div>
+          <div className="rounded-lg border-gray-300 border-2 mb-10 p-4">
+            <h3 className="text-center py-4 underline font-bold text-lg">
+              Completed Tasks
+            </h3>
+            {todos.filter((todo) => todo.done).length ? (
+              <ul>
+                {todos
+                  .filter((todo) => todo.done)
+                  .map((todo, index) => (
+                    <li
+                      key={index}
+                      className="rounded-lg border-gray-200 border-2 my-4 flex p-3 "
+                    >
+                      <FontAwesomeIcon
+                        icon={faArrowUp}
+                        onClick={() => moveUp(todo)}
+                        className="mx-1 p-3 text-teal-600 cursor-pointer"
+                      />
+                      <FontAwesomeIcon
+                        icon={faArrowDown}
+                        onClick={() => moveDown(todo)}
+                        className=" mx-1 p-3 text-teal-600 cursor-pointer"
+                      />
+                      <span className="flex-grow line-through mx-1 p-3">
+                        {todo.todo}
+                      </span>
+                      <div className="todo-btns">
+                        <FontAwesomeIcon
+                          icon={faTimes}
+                          onClick={() => markDone(todo)}
+                          className="text-teal-600 cursor-pointer mx-1 p-3"
+                        />
+
+                        <FontAwesomeIcon
+                          icon={faTrashCan}
+                          onClick={() => deleteTodo(todo)}
+                          className="cursor-pointer p-3 mx-1 text-red-600  w-5"
+                        />
+                      </div>
+                    </li>
+                  ))}
+              </ul>
+            ) : (
+              <p>You have not completed any tasks yet.</p>
+            )}
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
