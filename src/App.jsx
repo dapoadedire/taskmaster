0c0cf9394521182cf55f145986604059b500dc4e
dapoadedire <adedireadedapo19@gmail.com>
2023-01-15 11:21:37 +0100

    fix: final fix

--- src/App.jsx
+++ src/App.jsx
@@ -75,13 +75,17 @@ function App() {
 
   return (
     <div className="font-mono">
+
+   
       <header className="h-32">
-        <h1 className="flex h-full items-center justify-center text-center text-5xl font-extrabold">Todo List</h1>
+        <h1 className="flex h-full items-center justify-center text-center text-5xl font-extrabold">
+          TaskMaster 
+        </h1>
       </header>
       <main>
-        <div className="mx-auto  w-3/4" ref={parent}>
+        <div className="mx-auto  w-full px-4 sm:flex sm:w-3/4 sm:flex-wrap sm:gap-10 lg:w-3/5">
           <form
-            className="mb-20 flex"
+            className="mb-20 flex sm:w-full sm:grow"
             onSubmit={(e) => {
               e.preventDefault();
               addTodo(todo);
@@ -93,18 +97,20 @@ function App() {
               placeholder="Add a new task"
               onChange={(e) => setTodo(e.target.value)}
               required="false"
-              className="grow rounded-lg border-2 border-gray-300 p-4 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-teal-600"
+              className="grow rounded-lg border-2 border-gray-300 p-3 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-teal-600"
             />
 
             <button
               type="submit"
-              className="ml-3 w-20 rounded-lg border-2 border-teal-700 bg-teal-700 p-4 text-xl font-bold text-white hover:bg-teal-600 hover:text-white"
+              className="ml-3 w-20 rounded-lg border-2 border-teal-700 bg-teal-700 p-2 text-xl font-bold text-white hover:bg-teal-600 hover:text-white"
             >
               <FontAwesomeIcon icon={faAdd} className="font-bold" />
             </button>
           </form>
 
-          <div className="mb-10 rounded-lg border-2 border-gray-300 p-4">
+          {/* pending tasks */}
+
+          <div className="mb-10 rounded-lg border-2 border-gray-300 p-2 sm:grow">
             <h3 className="py-4 text-center text-lg font-bold underline">
               Pending Tasks
             </h3>
@@ -118,41 +124,44 @@ function App() {
                   .map((todo, index) => (
                     <li
                       key={index}
-                      className="my-4 flex items-center rounded-lg border-2 border-gray-200 p-2 "
+                      className="my-4 flex items-center rounded-lg border-2 border-gray-200 p-2 hover:bg-gray-100 sm:mx-2"
                     >
                       <FontAwesomeIcon
                         icon={faArrowUp}
                         onClick={() => moveUp(todo)}
-                        className="mx-1 cursor-pointer rounded-lg p-4 text-teal-600 hover:bg-teal-100"
+                        className="mx-1 cursor-pointer rounded-lg p-2 text-teal-600 hover:bg-teal-100 sm:p-3"
                       />
                       <FontAwesomeIcon
                         icon={faArrowDown}
                         onClick={() => moveDown(todo)}
-                        className=" mx-1 cursor-pointer rounded-lg p-4  text-teal-600 hover:bg-teal-100"
+                        className=" mx-1 cursor-pointer rounded-lg p-2  text-teal-600 hover:bg-teal-100 sm:p-3"
                       />
-                      <span className="mx-1 grow p-4">{todo.todo}</span>
+                      <span className="mx-1 grow p-2">{todo.todo}</span>
 
                       <FontAwesomeIcon
                         icon={faCheck}
                         onClick={() => markDone(todo)}
-                        className="mx-1 ml-2 w-5 cursor-pointer rounded-lg p-4  text-teal-600 hover:bg-teal-200"
+                        className="mx-1 ml-2 w-5 cursor-pointer rounded-lg p-2  text-teal-600 hover:bg-teal-200 sm:p-3"
                       />
 
                       <FontAwesomeIcon
                         icon={faTrashCan}
                         onClick={() => deleteTodo(todo)}
-                        className="mx-1 cursor-pointer rounded-lg p-4  text-red-600 hover:bg-red-100" 
+                        className="mx-1 cursor-pointer rounded-lg p-2  text-red-600 hover:bg-red-100 sm:p-3" 
                       />
                     </li>
                   ))}
               </ul>
             ) : (
-              <p className="my-4 flex rounded border-2 border-gray-200 p-4">
+              <p className="my-4 flex rounded-lg border-2 border-gray-200 p-2 text-center">
                 You have no pending tasks. Why not add one?
               </p>
             )}
           </div>
-          <div className="mb-10 rounded-lg border-2 border-gray-300 p-4">
+          
+
+          {/* Completed todos */ }
+          <div className="mb-10 rounded-lg border-2 border-gray-300 p-2 sm:grow">
             <h3 className="py-4 text-center text-lg font-bold underline">
               Completed Tasks
             </h3>
@@ -163,39 +172,50 @@ function App() {
                   .map((todo, index) => (
                     <li
                       key={index}
-                      className="my-4 flex rounded-lg border-2 border-gray-200 p-4 "
+                      className=" my-4 flex rounded-lg border-2 border-gray-200 p-2 hover:bg-gray-100 sm:mx-2 "
                     >
                       <FontAwesomeIcon
                         icon={faArrowUp}
                         onClick={() => moveUp(todo)}
-                        className="mx-1 cursor-pointer rounded-lg p-4  text-teal-600 hover:bg-teal-100"
+                        className="mx-1 cursor-pointer rounded-lg p-2  text-teal-600 hover:bg-teal-100 sm:p-3
+                       
+                        "
+                      
                       />
                       <FontAwesomeIcon
                         icon={faArrowDown}
                         onClick={() => moveDown(todo)}
-                        className=" mx-1 cursor-pointer rounded-lg p-4  text-teal-600 hover:bg-teal-100"
+                        className=" mx-1 cursor-pointer rounded-lg p-2  text-teal-600 hover:bg-teal-100 sm:p-3"
                       />
-                      <span className="mx-1 grow p-4 line-through">
+                      <span className="mx-1 grow p-2 line-through">
                         {todo.todo}
                       </span>
                       <div className="">
                         <FontAwesomeIcon
                           icon={faTimes}
                           onClick={() => markDone(todo)}
-                          className="mx-1 cursor-pointer rounded-lg p-4  text-teal-600 hover:bg-teal-200"
+                          className="mx-1 cursor-pointer rounded-lg p-2  text-teal-600 hover:bg-teal-200 sm:p-3"
                         />
-
+                        <FontAwesomeIcon
+                          icon={faTrashCan}
+                          onClick={() => deleteTodo(todo)}
+                          className="mx-1 cursor-pointer rounded-lg p-2  text-red-600 hover:bg-red-100 sm:p-3"
+                        />
                        
                       </div>
                     </li>
                   ))}
               </ul>
             ) : (
-              <p>You have not completed any tasks yet.</p>
+                <p className="my-4 flex rounded-lg border-2 border-gray-200 p-2 text-center">
+                  You have no completed tasks.
+                </p>
             )}
           </div>
         </div>
+     
       </main>
+    
     </div>
   );
 }
